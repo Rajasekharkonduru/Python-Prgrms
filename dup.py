2bd098d939dbac36ef715cbcee9e12ebb2cf5dcb
Rajasekharkonduru <37657026+Rajasekharkonduru@users.noreply.github.com>
2018-04-09 18:21:00 +0530

    Add files via upload

---
+++ dup.py
@@ -0,0 +1,11 @@
+l=[1,2,3,1,2,3,4,4,5,6]
+a=l
+for i in range(len(l)):
+   b=l[i]
+   for j in b:
+      if(j==a[i]):
+         print(j)
+   
+     
+
+
---
+++ online.py
@@ -0,0 +1,6 @@
+l=["seafood", "nuts", "dairy", "chicken"]
+a=input("enter somthing eaten in last 24hr:")
+if (a==l[i]):
+    print("true",a ,"available")
+else:
+    print("false")
